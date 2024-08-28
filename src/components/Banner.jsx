a0b5320e41c41c5caeffe635319ca8b86ceb5483
tatianasnook <tatsnook@gmail.com>
2024-08-28 13:32:14 -0500

    assignment Orbit report is completed

---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+
+const Banner = () => {
+  return (
+    <div>
+      <header>
+        <h1>Orbit Report</h1>
+      </header>
+      <p>Click on the buttons to see the satellites in that orbit type</p>
+    </div>
+    
+  )
+}
+
+export default Banner;
